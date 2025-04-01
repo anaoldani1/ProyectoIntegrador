4f651c02ae63d5d2a2d028045d241b63ae3ad869
anaoldani1 <oldania@udesa.edu.ar>
2025-04-01 15:29:51 -0300

    hola

---
+++ db/users.js
@@ -0,0 +1,8 @@
+const usuario = {
+    email: "pepeluis1@gmail.com",
+    usuario: "pepeluis1",
+    password: "pepeluis1",
+    fechaNacimiento: "10/01/2006",
+    documento: 11111111,
+    fotoPerfil: "fotoPerfil.png"   
+}
