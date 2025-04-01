3877d132ca14cfd967608d91a7ddf4859f674927
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-04-01 12:12:28 -0300

    Cree el formulario de register

--- proyectoIntegrador/routes/users.js
+++ proyectoIntegrador/routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+///para el formulario de register///
+router.get("/register", function(req, res) {
+  res.render("register");
+});
+////termina register 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
