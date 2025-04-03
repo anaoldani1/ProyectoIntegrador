76923106c4cc2d11dec5027a08265def842fa821
anaoldani1 <oldania@udesa.edu.ar>
2025-04-03 10:57:53 -0300

    cambios

--- proyectoIntegrador/routes/users.js
+++ proyectoIntegrador/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let userController = require('../controllers/userController')
 
 ///para el formulario de register///
 router.get("/register", function(req, res) {
@@ -7,9 +8,8 @@ router.get("/register", function(req, res) {
 });
 ////termina register 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//para usar datos de usuario en distintas pags//
+router.get('/', userController.nombreDeUsuario);
+///
 
 module.exports = router;
