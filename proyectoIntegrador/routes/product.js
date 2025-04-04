8ae14d6b08eb757eae4ef3d920704e1e5bd1418e
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-04-04 15:53:14 -0300

    as

--- proyectoIntegrador/routes/product.js
+++ proyectoIntegrador/routes/product.js
@@ -1,24 +1,10 @@
 // //// para el punto 10 de detalle////
-// const express = require("express");
-// const router = express.Router();
-// const productController = require("../controllers/productController");
+const express = require("express");
+const router = express.Router();
+const productController = require("../controllers/productController");
 
-// router.get("/detalle", productController.detalle);
-
-// module.exports = router;
-
-
-
-
+router.get("/", productController.index);
+router.get("/id/:id", productController.filtrarId);
 
 module.exports = router;
-// module.exports = router;
-// var express = require('express');
-// var router = express.Router();
-// var productController = require('../controllers/productController');
-
-// // Ruta para ver el detalle del producto con un ID dinámico
-// router.get('/', productController.index);
-
-// module.exports = router;
 
