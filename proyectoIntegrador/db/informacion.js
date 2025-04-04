5d1f95b420e0d61138616e123e60e5a071ea2dc8
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-04-04 16:44:29 -0300

    Agrego comentarios de productos

--- proyectoIntegrador/db/informacion.js
+++ proyectoIntegrador/db/informacion.js
@@ -90,7 +90,60 @@ const informacion = {
 
             
         },
+        comentarios= [
+                {
+                    nombreUsuario: "brian123",
+                    fotoUsuario: "brian.png",
+                    comentario: "Me encanta este modelo, siempre quise uno."
+                },
+                {
+                    nombreUsuario: "tati123",
+                    fotoUsuario: "tati.png",
+                    comentario: "No es de mi estilo, pero tiene buen diseño."
+                },
+                {
+                    nombreUsuario: "ana123",
+                    fotoUsuario: "ana.png",
+                    comentario: "¡Se ve espectacular! ¿Qué año es?"
+                },
+                {
+                    nombreUsuario: "carlos87",
+                    fotoUsuario: "carlos.png",
+                    comentario: "No me convence el color, pero seguro es rápido."
+                },
+                {
+                    nombreUsuario: "marco22",
+                    fotoUsuario: "marco.png",
+                    comentario: "Lo vi en la calle y suena increíble."
+                },
+                {
+                    nombreUsuario: "luli99",
+                    fotoUsuario: "luli.png",
+                    comentario: "Mi hermano tenía uno igual, nostalgia pura."
+                },
+                {
+                    nombreUsuario: "pepe77",
+                    fotoUsuario: "pepe.png",
+                    comentario: "Muy buen auto, pero seguro es carísimo."
+                },
+                {
+                    nombreUsuario: "sofia_love",
+                    fotoUsuario: "sofia.png",
+                    comentario: "Se ve muy elegante, me gusta el diseño."
+                },
+                {
+                    nombreUsuario: "dani_pro",
+                    fotoUsuario: "dani.png",
+                    comentario: "¿Alguien sabe qué motor tiene?"
+                },
+                {
+                    nombreUsuario: "fercho",
+                    fotoUsuario: "fercho.png",
+                    comentario: "Yo tuve uno similar y era una joya."
+                }
+            ] 
     ],
+    
     filtrarPorSearch: function(busqueda){
         let listaBusqueda=[]
         for (let index = 0; index < this.productos.length; index++) {
