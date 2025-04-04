be0d84904937388b68605bce1a474bbc87104c9d
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-04-04 17:02:39 -0300

    changes

--- proyectoIntegrador/db/informacion.js
+++ proyectoIntegrador/db/informacion.js
@@ -93,52 +93,52 @@ const informacion = {
         comentarios= [
                 {
                     nombreUsuario: "brian123",
-                    fotoUsuario: "brian.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Me encanta este modelo, siempre quise uno."
                 },
                 {
                     nombreUsuario: "tati123",
-                    fotoUsuario: "tati.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "No es de mi estilo, pero tiene buen diseño."
                 },
                 {
                     nombreUsuario: "ana123",
-                    fotoUsuario: "ana.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "¡Se ve espectacular! ¿Qué año es?"
                 },
                 {
                     nombreUsuario: "carlos87",
-                    fotoUsuario: "carlos.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "No me convence el color, pero seguro es rápido."
                 },
                 {
                     nombreUsuario: "marco22",
-                    fotoUsuario: "marco.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Lo vi en la calle y suena increíble."
                 },
                 {
                     nombreUsuario: "luli99",
-                    fotoUsuario: "luli.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Mi hermano tenía uno igual, nostalgia pura."
                 },
                 {
                     nombreUsuario: "pepe77",
-                    fotoUsuario: "pepe.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Muy buen auto, pero seguro es carísimo."
                 },
                 {
                     nombreUsuario: "sofia_love",
-                    fotoUsuario: "sofia.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Se ve muy elegante, me gusta el diseño."
                 },
                 {
                     nombreUsuario: "dani_pro",
-                    fotoUsuario: "dani.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "¿Alguien sabe qué motor tiene?"
                 },
                 {
                     nombreUsuario: "fercho",
-                    fotoUsuario: "fercho.png",
+                    fotoUsuario: "/images/users/default-image.png",
                     comentario: "Yo tuve uno similar y era una joya."
                 }
             ] 
