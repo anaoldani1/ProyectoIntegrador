a2217fefc46b3bf584f9e4d624d0e2166cfeb394
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-04-07 18:30:44 -0300

    productos sql

--- proyectoIntegrador/db/Usuarios.sql
+++ proyectoIntegrador/db/Usuarios.sql
@@ -64,4 +64,123 @@ INSERT INTO usuarios VALUES(
     "/foto.png",
     null,
     null,
-    null)
+    null);
+
+CREATE TABLE productos(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    imagen VARCHAR (300),
+    nombreProducto VARCHAR(250),
+    descripcion TEXT,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedAt  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	deletedAt  TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    usuarioId INT UNSIGNED,
+    FOREIGN KEY (usuarioId) REFERENCES usuarios(id)
+    
+);
+
+INSERT INTO productos VALUES(
+DEFAULT,
+"/foto.png",
+"Auto Ferrari",
+"Exterior Blanco",
+null,
+null,
+null,
+1
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto Mclaren",
+"Exterior negro",
+null,
+null,
+null,
+1
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto mustang",
+"Exterior gris",
+null,
+null,
+null,
+2
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto BM",
+"Exterior marron",
+null,
+null,
+null,
+2
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto Toyota",
+"Exterior rosa",
+null,
+null,
+null,
+3
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto fiat",
+"Exterior celeste",
+null,
+null,
+null,
+3
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto mercedes",
+"Exterior rojo",
+null,
+null,
+null,
+4
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto alfa romeo",
+"Exterior azul",
+null,
+null,
+null,
+4
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto audi",
+"Exterior violeta",
+null,
+null,
+null,
+5
+),
+(
+DEFAULT,
+"/foto.png",
+"Auto Land Rover",
+"Exterior amarillo",
+null,
+null,
+null,
+5
+);
+
+
+
+
+
