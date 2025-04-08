f7547edc7c945de78df4fbaa4bdb66e60be1dfeb
anaoldani1 <oldania@udesa.edu.ar>
2025-04-08 17:13:21 -0300

    s

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -4,13 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mainRouter = require('./routes/index');
 var productRouter = require("./routes/product");
-var loginRouter = require("./routes/login");
-var profileRouter = require("./routes/profile");
-var registerRouter = require("./routes/register");
-var searchResultsRouter = require("./routes/search-results");
-var productAddRouter = require('./routes/productadd')
+var userRouter = require("./routes/login");
 
 var app = express();
 
@@ -26,11 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use("/products", productRouter);
-app.use('/login', loginRouter);
-app.use('/profile', profileRouter);
-app.use('/register', registerRouter);
-app.use('/searchresults', searchResultsRouter);
-app.use('/productadd', productAddRouter);
+app.use('/users', loginRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
