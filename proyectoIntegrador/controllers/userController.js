995560c0d4d8d506609f1d70e2cd86808167bf87
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-04-14 15:36:07 -0300

    comentando el codigo

--- proyectoIntegrador/controllers/userController.js
+++ proyectoIntegrador/controllers/userController.js
@@ -1,15 +1,14 @@
-const informacion = require('../db/informacion')
+const informacion = require('../db/informacion') // requiere la informadion de db (el objeto literal informacion)
 
-const userController = {
-    login: function(req, res){
-        return res.render('login', 
-            {usuario: informacion.usuario,
-                usuario: informacion.usuarios, 
-                productos: informacion.productos,
+const userController = { // creamos un objeto literal para luego exportar
+    login: function(req, res){  
+        return res.render('login', // datos enviados a login.ejs para renderizarlos
+            {usuario: informacion.usuarios, 
+            productos: informacion.productos,
         })
     },
     register: function (req, res) {
-        return res.render('register', 
+        return res.render('register', // datos enviados a registes.ejs para renderizarlos
             {datos: informacion.usuarios,
         })
     },
--- proyectoIntegrador/routes/user.js
+++ proyectoIntegrador/routes/user.js
@@ -1,8 +1,9 @@
+// requerimos express
 const express = require('express');
 const router = express.Router();
 const userController= require('../controllers/userController')
 
-router.get('/login', userController.login);
+router.get('/login', userController.login); 
 
 router.get('/register', userController.register);
 
