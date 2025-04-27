9e61556630f97e4d548a22a2a164c423cc06be0d
Agustina-Cavallaro <mcavallaro@udesa.edu.ar>
2025-04-27 09:41:36 -0300

    Comentando el codigo

--- proyectoIntegrador/controllers/mainController.js
+++ proyectoIntegrador/controllers/mainController.js
@@ -1,21 +1,23 @@
 //puente entre las vistas y los modelos, los metodos de cada controlador reciben los pedidos y procesan informacion
 
+//importa de db toda la informacion de los productos
 const informacion = require('../db/informacion');
 
+///Creo un objeto literal que tiene dos métodos: index y searchResults.
 const mainController = {
     index: function (req, res) {  
-        //envia informacion de productos a index.ejs para renderizarla, lista de objetos literales con strings          
+        //envia informacion de productos a index.ejs para renderizarla, lista de objetos literales con strings, y manda productos que puede mostrar         
         return res.render("index", { 
             productos: informacion.productos,
         });
     },
     searchResults: function (req, res) {
-        /// //envia informacion de productos a search-results.ejs para renderizarla, lista de objetos literales con strings
+        ///Maneja la ruta de resultados de búsqueda (/search). Renderiza la vista search-results.ejs, y también le manda los productos para que los pueda mostrar.
         return res.render("search-results", { 
             productos: informacion.productos,
         });
     },
 
 };
-
+///Exporto el controlador para poder usarlo en el archivo de rutas (routes/mainRoutes.js).
 module.exports = mainController;
--- proyectoIntegrador/controllers/productController.js
+++ proyectoIntegrador/controllers/productController.js
@@ -1,21 +1,27 @@
 ////PARTE DE DETALLE--> 10
+
+//Importa los datos de productos, usuarios y comentarios 
 const informacion = require("../db/informacion")
+
+//creo un objeto literal que contiene tres métodos: index, filtrarId, y add.
 const productController= {
     index: function(req,res) {
         //envia la informacion de productos a product.ejs para poder renderizarlo, es un array de objetos literales
         return res.render("product", {
+            //le pasa todos los productos
             info: informacion.productos
         }) 
     },
 
     filtrarId: function (req,res) {
-        let idBuscado= req.params.id;
-        let ObjAnswer= informacion.filtrarId(idBuscado)
+        //Busca un producto específico basado en el id que recibe en la URL (req.params.id)
+        let idBuscado= req.params.id; //Agarra el id que viene en la URL.
+        let ObjAnswer= informacion.filtrarId(idBuscado) //Usa  función que está en informacion.js filtrarId. que busca en la lista cual tiene ese id y lo duvelve
         
         return res.render("product",
         {
-            detalle: ObjAnswer,
-            comentarios: informacion.comentarios
+            detalle: ObjAnswer, //le manda el producto q encontramos
+            comentarios: informacion.comentarios //los comentarios q vienem de ahi
         })
     },
 
