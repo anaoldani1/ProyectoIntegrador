b019ad4f5d29f2ab378304945b23bc4421e4109f
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-05-19 18:28:04 -0300

    config js hecho y product.js de database terminado y hice el include en product controllers

--- proyectoIntegrador/database/config/config.js
+++ proyectoIntegrador/database/config/config.js
@@ -1,23 +1,10 @@
-{
+module.exports={ // agrego module.exports a el objeto litetal
   "development": {
     "username": "root",
-    "password": null,
-    "database": "database_development",
+    "password": "root", // cambio null a "root"
+    "database": "usuarios", // pongo el nombre de la base de datos 
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+
 }
--- proyectoIntegrador/database/models/product.js
+++ proyectoIntegrador/database/models/product.js
@@ -1,3 +1,47 @@
 module.export=function (sequalize,dataTypes) {
     let alias = "Product"
+    let cols = {
+        id: {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.INTEGER
+        },
+        imagen: {
+            type : dataTypes.STRING
+        },
+        nombreProducto: {
+            type : dataTypes.STRING
+        },
+        descripcion: {
+            type : dataTypes.STRING
+        },
+        createdAt: {
+            type: dataTypes.DATE
+        },
+        updated_at: {
+            type: dataTypes.DATE
+        },
+        deletedAt: {
+            type: dataTypes.DATE
+        },
+        usuarioId: {
+            type: dataTypes.INTEGER
+        }
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: false,
+    }
+
+    let Product = sequelize.define(alias, cols, config);
+
+    Product.associate = function (models) {
+        Product.belongsTo(models.User,{
+            as:"User",
+            foreignKey: "usuarioId"
+        })
+    }
+
+
 }
