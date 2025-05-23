3f3249ae0372dab845056b745278c8d61f72c044
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-05-23 09:41:57 -0300

    login punto 2

--- proyectoIntegrador/routes/user.js
+++ proyectoIntegrador/routes/user.js
@@ -16,4 +16,8 @@ router.get('/profile', userController.profile);
 ///2 parte register
 router.post('/register', userController.processRegister);
 
+
+///2 parte login
+router.post("/login", userController.processLogin);
+
 module.exports = router; 
