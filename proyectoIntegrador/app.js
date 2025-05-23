4c71df48579151cf83d6b38d62c3608c48dfca16
anaoldani1 <oldania@udesa.edu.ar>
2025-05-23 15:34:13 -0300

    profile

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -50,10 +50,6 @@ app.use('/', mainRouter); //Todas las rutas que empiezan con /se manejan con mai
 app.use("/product", productRouter); //Todas las rutas que empiezan con /product se manejan con productRouter.//
 app.use('/user', userRouter);  //Todas las rutas que empiezan con /user (por ejemplo, /user/login, /user/register) se manejan con userRouter.//
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7784818910008aebfab572088bf2c364f136a06b
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
