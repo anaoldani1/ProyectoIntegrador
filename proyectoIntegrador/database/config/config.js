188c34d893ed35ff7f1b2e86f6e7b4ef6012c101
anaoldani1 <oldania@udesa.edu.ar>
2025-05-24 17:05:45 -0300

    resolviendo errores de las paginas

---
+++ proyectoIntegrador/database/config/config.js
@@ -0,0 +1,10 @@
+module.exports={ // agrego module.exports a el objeto litetal
+  "development": {
+    "username": "root",
+    "password": "root", // cambio null a "root"
+    "database": "proyectoIntegrador", // pongo el nombre de la base de datos 
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+
+}
