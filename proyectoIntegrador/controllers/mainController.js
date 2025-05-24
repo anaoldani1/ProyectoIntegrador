ef146ccc8e1dc081f21a3c05fd7b52520495a1bc
Nicole de Leone <ndeleone@udesa.edu.ar>
2025-05-24 16:40:15 -0300

    product ejs

--- proyectoIntegrador/controllers/mainController.js
+++ proyectoIntegrador/controllers/mainController.js
@@ -1,43 +1,60 @@
+
 //puente entre las vistas y los modelos
 //los metodos de cada controlador reciben los pedidos y procesan informacion
 
-//importa de db toda la informacion de los productos
 
-// importar db y Op 
+//importa de db toda la informacion de los productos
+const informacion = require('../db/informacion');
+// importar db y Op
 let db = require("../database/models");
 let op = db.Sequelize.Op;
-    //req y res son metodos que contienen objetos literales
-    ///Creo un objeto literal que tiene dos métodos: index y searchResults.
+   //req y res son metodos que contienen objetos literales
+   ///Creo un objeto literal que tiene dos métodos: index y searchResults.
 const mainController = {
-    //indexx muestra el inicio 
-    index: function (req, res) {  
-        //envia informacion de productos a index.ejs para renderizarla, lista de objetos literales con strings       
-        return res.render("index", { 
-            productos: informacion.productos, ///accedes a productos y mandas eso 
-        });
-    },
-    //muestra los resultados de busqeda 
-    searchResults: function (req, res) {
-        ///Maneja la ruta de resultados de búsqueda (/search). Renderiza la vista search-results.ejs, y también le manda los productos para que los pueda mostrar.
+   //indexx muestra el inicio
+   index: function (req, res) { 
+       //envia informacion de productos a index.ejs para renderizarla, lista de objetos literales con strings      
+      
+       db.Product.findAll( {
+           include: [{ association: "usuario" }]
+         })
 
-        let searchTerm = req.query.search
 
-        db.Product.findAll({
-            where: [{nombreProducto: {[op.like] : "%" + searchTerm + "%"}}],
-            include: [{association: "usuario"}]
-        })
-        .then(function (resultados) {
-            let mensaje= undefined
-            if (resultados.length===0) {
-                mensaje="   No hay resultados para su criterio de búsqueda"
-                return res.render("search-results",{productos:resultados, mensaje: mensaje})
-            }else{
-                return res.render("search-results",{productos:resultados, mensaje: mensaje})
-            }
-        })
-    },
+       .then(function (resultados) {
+           return res.render("index",{products:resultados})
+       })
+
+
+   },
+   //muestra los resultados de busqeda
+   searchResults: function (req, res) {
+       ///Maneja la ruta de resultados de búsqueda (/search). Renderiza la vista search-results.ejs, y también le manda los productos para que los pueda mostrar.
+
+
+       let searchTerm = req.query.search
+
+
+       db.Product.findAll({
+           where: [{nombreProducto: {[op.like] : "%" + searchTerm + "%"}}],
+           include: [{association: "usuario"}]
+       })
+       .then(function (resultados) {
+           let mensaje= undefined
+           if (resultados.length===0) {
+               mensaje="   No hay resultados para su criterio de búsqueda"
+               return res.render("search-results",{productos:resultados, mensaje: mensaje})
+           }else{
+               return res.render("search-results",{productos:resultados, mensaje: mensaje})
+           }
+       })
+   },
+
 
 };
 
+
 ///Exporto el controlador para poder usarlo en el archivo de rutas (routes/mainRoutes.js).
 module.exports = mainController;
+
+
+
